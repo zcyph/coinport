1cba36fff3ce68f805ebf69ca19d5ba42174672d
zcyph <github@donairmail.com>
2021-01-31 01:26:56 -0700

    formatting

--- coinport.py
+++ coinport.py
@@ -2,6 +2,7 @@ import locale, time
 from pycoingecko import CoinGeckoAPI
 from pyexcel_ods import get_data
 from os import system, name
+from termcolor import colored
 
 def clear():
     # for windows
@@ -17,10 +18,10 @@ while True:
     locale.setlocale(locale.LC_ALL, '')
 
     # define symbols
-    symbols = {'BTC': 'bitcoin', 'ETH': 'ethereum', 'XMR': 'monero', 'ADA': 'cardano', 'XLM': 'stellar', 'ZEC': 'zcash', 'LINK': 'chainlink', 'COMP': 'compound-governance-token', 'GLM': 'golem', 'SNX': 'havven', 'UNI': 'uniswap', 'NANO': 'nano', 'DOGE': 'dogecoin'}
+    symbols = {'BTC': 'bitcoin', 'ETH': 'ethereum', 'XMR': 'monero', 'ADA': 'cardano', 'XLM': 'stellar', 'ZEC': 'zcash', 'LINK': 'chainlink', 'COMP': 'compound-governance-token', 'GLM': 'golem', 'SNX': 'havven', 'UNI': 'uniswap', 'NANO': 'nano', 'DOT': 'polkadot', 'XRP': 'ripple', 'DOGE': 'dogecoin'}
 
     # retrieve prices from CoinGecko API, flatten with dictionary comprehension
-    getprices = CoinGeckoAPI().get_price(ids='bitcoin,ethereum,monero,cardano,stellar,zcash,chainlink,compound-governance-token,golem,havven,uniswap,nano,dogecoin', vs_currencies='cad')
+    getprices = CoinGeckoAPI().get_price(ids='bitcoin,ethereum,monero,cardano,stellar,zcash,chainlink,compound-governance-token,golem,havven,uniswap,nano,polkadot,ripple,dogecoin', vs_currencies='cad')
     prices = {k:getprices[v]['cad'] for (k,v) in symbols.items()}
 
     # get current holdings & book value from spreadsheet, convert nested OrderedDict to dictionary
@@ -32,17 +33,19 @@ while True:
     holdings_value = {k:(holdings[k] * prices[k]) for k in holdings}
     total_holdings = sum(holdings_value.values())
 
-    # display current prices for all held coins
-    for i in symbols:
-        print(f"| {i}: " + locale.currency(prices[i]), end=" |")
+    # # display current prices for all held coins
+    # for i in symbols:
+    #     print(f"| {i}: " + locale.currency(prices[i]), end=" |")
 
     # display portfolio book/market value and profit/loss in dollars
-    print(f"\n\nPORTFOLIO:\n")
-    print(f"BOOK VALUE: " + locale.currency(bookvalue))
-    print(f"MARKET VALUE: " + locale.currency(total_holdings))
-    print(f"NET P/L: " + locale.currency(total_holdings - bookvalue) + "\n")
+    print(colored(f"P/L: ", "yellow", attrs=['bold']) + colored(locale.currency(total_holdings - bookvalue), "green") + colored("\nBOOK: ", "yellow", attrs=['bold']) + locale.currency(bookvalue) + colored("\nMARKET: ", "yellow", attrs=['bold']) + locale.currency(total_holdings) + "\n")
+    print(colored("Symbol   Price       Balance              Value", "yellow", attrs=['bold']))
+    print(colored("--------------------------------------------------", "cyan"))
 
     # display individual holdings in cryptocurrency and dollars
     for i in symbols:
-        print(f"{i}: {holdings[i]} ({locale.currency(holdings_value[i])})")
+        spaces = (6 - len(i)) * ' '
+        spaces2 = (10 - len(str(locale.currency(prices[i])))) * ' '
+        spaces3 = (20 - len(str(holdings[i]))) * ' '
+        print(colored(f"{i}:", "green") + (f"{spaces}  {locale.currency(prices[i])} {spaces2} {holdings[i]} {spaces3}" + colored(locale.currency(holdings_value[i]), "cyan")))
     time.sleep(5)
