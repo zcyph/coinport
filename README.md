33e36468e1922bf0048a0e98aba986db8118b822
zcyph <github@donairmail.com>
2021-02-10 21:52:34 -0700

    new version

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # coinport
-coinport is a minimalist cryptocurrency tracker written in Python, leveraging the CoinGecko API to show prices of your favorite cryptocurrencies.
+coinport is a minimalist cryptocurrency tracker written in Python, leveraging the CoinGecko API to track prices of your favorite cryptocurrencies.
 
-It also retrieves information from a spreadsheet to display balance of your holdings, book value, market value and profit/loss.
+It retrieves information from a spreadsheet to display the balance of your holdings, its book value, market value and profit/loss.
 
 ## Requirements
 
@@ -11,9 +11,11 @@ This is a Python script, so make sure you have Python installed. This was tested
 
 **Dependencies**
 
-A few things are required in order to run this script. After installing Python, do the following commands to install the dependencies. It is a good practice to do this within a virtual environment:
+A few things are required in order to run this script. After installing Python, do the following commands to install the dependencies. Whilte not required, it is a good practice to do this within a virtual environment:
 
 `pip install pycoingecko`
+
+`pip install pandas`
 
 `pip install pyexcel_ods`
 
@@ -23,9 +25,19 @@ Depending on your environment, you may or may not need to type "pip3" instead of
 
 ## Usage
 
-Make sure the spreadsheet is in the same folder as the script, run it like this: 
+Download ZIP above or just git clone the repo:
+
+`git clone https://github.com/zcyph/coinport`
+
+Change to the directory you just downloaded into, ie:
+
+`cd coinport`
+
+Edit the included Excel spreadsheet to your liking and then run the script:
 
 `python coinport.py`
+
+The script will stay running unless you stop it (you can do that with CTRL-C). It will automatically refresh frequently to retrieve updated prices. If you save changes to the spreadsheet it will automatically update with your new additions.
 
 
 ![](https://github.com/zcyph/coinport/blob/main/screenshot.png)
--- coinport.py
+++ coinport.py
@@ -1,48 +1,103 @@
-import locale, time
+import locale, time, json
+import pandas as pd
 from pycoingecko import CoinGeckoAPI
-from pyexcel_ods import get_data
-from os import system, name
+from os import system, name, path
 from termcolor import colored
 
+################################################# configuration #
+coin_list = 'coinlist.json'
+coinport_config = 'conf.json'
+with open(coinport_config) as config_file:
+    coinport_config_data = json.load(config_file)
+    spreadsheet = coinport_config_data['spreadsheet']['filepath']
+cg = CoinGeckoAPI()
 def clear():
-    # for windows
     if name == 'nt':
         _ = system('cls')
-    # for mac and linux(here, os.name is 'posix')
     else:
         _ = system('clear')
+# configuration #################################################
 
+# keep program running, prompt user for spreadsheet filepath if not found
 while True:
-    # clear screen & set locale for currency formatting
-    clear()
-    locale.setlocale(locale.LC_ALL, '')
+    if path.isfile(spreadsheet):
+        while True:
+                # clear screen, setlocale to format currency
+                clear()
+                locale.setlocale(locale.LC_ALL, '')
 
-    # define symbols
-    symbols = {'BTC': 'bitcoin', 'ETH': 'ethereum', 'XMR': 'monero', 'ADA': 'cardano', 'XLM': 'stellar', 'ZEC': 'zcash', 'LINK': 'chainlink', 'COMP': 'compound-governance-token', 'GLM': 'golem', 'SNX': 'havven', 'UNI': 'uniswap', 'NANO': 'nano', 'DOT': 'polkadot', 'XRP': 'ripple', 'DOGE': 'dogecoin'}
+                # get list of coins from CoinGecko
+                allcoins = cg.get_coins_list()
 
-    # retrieve prices from CoinGecko API, flatten with dictionary comprehension
-    getprices = CoinGeckoAPI().get_price(ids='bitcoin,ethereum,monero,cardano,stellar,zcash,chainlink,compound-governance-token,golem,havven,uniswap,nano,polkadot,ripple,dogecoin', vs_currencies='cad')
-    time.sleep(1)
-    prices = {k:getprices[v]['cad'] for (k,v) in symbols.items()}
+                # spreadsheet -> Pandas df (dataframe)
+                df = pd.ExcelFile(spreadsheet).parse(pd.ExcelFile(spreadsheet).sheet_names[0])
 
-    # get current holdings & book value from spreadsheet, convert nested OrderedDict to dictionary
-    data = get_data("crypto.ods", start_row=0, row_limit=2, start_column=2)
-    holdings = dict(zip(data['Sheet1'][0], data['Sheet1'][1]))
-    bookvalue = (get_data("crypto.ods", start_row=1, row_limit=1, start_column=1, column_limit=1))['Sheet1'][0][0]
+                # portfolio coins to track
+                coins_to_track = df.columns[2:].tolist()
 
-    # current cryptocurrencies held, and total dollar amount
-    holdings_value = {k:(holdings[k] * prices[k]) for k in holdings}
-    total_holdings = sum(holdings_value.values())
+                # if not already there, download & save complete list of coins from CoinGecko
+                if not path.isfile(coin_list):
+                    with open('coinlist.json', 'w') as coinlist_file:
+                        json.dump(allcoins, coinlist_file, sort_keys=True, indent=4, separators=(',', ': '))
 
-    # display portfolio book/market value and profit/loss in dollars
-    print(colored(f"P/L: ", "yellow", attrs=['bold']) + colored(locale.currency(total_holdings - bookvalue), "green") + colored("\nBOOK: ", "yellow", attrs=['bold']) + locale.currency(bookvalue) + colored("\nMARKET: ", "yellow", attrs=['bold']) + locale.currency(total_holdings) + "\n")
-    print(colored("Symbol   Price       Balance              Value", "yellow", attrs=['bold']))
-    print(colored("---------------------------------------------------", "green", attrs=['bold']))
+                # functions to look up CoinGecko ID or ticker symbol with coin name
+                def name_to_id(coin_name):
+                    try:
+                        return next(i['id'] for i in allcoins if i["name"] == coin_name)
+                    except:
+                        print(f"Woops, {coin_name} doesn't seem to be there, please check spelling and case sensitivity")
 
-    # display individual holdings in cryptocurrency and dollars
-    for i in symbols:
-        spaces = (6 - len(i)) * ' '
-        spaces2 = (10 - len(str(locale.currency(prices[i])))) * ' '
-        spaces3 = (20 - len(str(holdings[i]))) * ' '
-        print(colored(f"{i}:", "yellow", attrs=['bold']) + (f"{spaces}  {locale.currency(prices[i])} {spaces2} {holdings[i]} {spaces3}" + colored(locale.currency(holdings_value[i]), "blue", attrs=['bold'])))
-    time.sleep(8)
+                def name_to_symbol(coin_name):
+                    try:
+                        return next(i['symbol'] for i in allcoins if i["name"] == coin_name)
+                    except:
+                        print(f"Woops, {coin_name} doesn't seem to be there, please check spelling and case sensitivity")
+
+                coin_ids = []
+                for coin in coins_to_track:
+                    coin_id = name_to_id(coin)
+                    coin_ids.append(coin_id)
+
+                symbols = {}
+                for coin in coins_to_track:
+                    coin_id = name_to_id(coin)
+                    symbol = name_to_symbol(coin)
+                    symbols.update({symbol.upper():coin_id})
+
+                prices = {}
+                for coin in coins_to_track:
+                    coin_id = name_to_id(coin)
+                    symbol = name_to_symbol(coin)
+                    prices.update({symbol.upper():cg.get_price(ids=coin_id, vs_currencies='cad')[coin_id]['cad']})
+
+                # retrieve current prices and flatten with dictionary comprehension
+                # prices = {k:(cg.get_price(ids=coin_ids, vs_currencies='cad'))[v]['cad'] for (k,v) in symbols.items()}
+
+                # get holdings and book value from spreadsheet
+                holdings = dict(zip(symbols.keys(),df.iloc[0:,2:].sum().to_list()))
+                total_book_value = df.iloc[0:,1].sum()
+
+                # current cryptocurrencies held, and total dollar amount
+                holdings_market_value = {k:(holdings[k] * prices[k]) for k in holdings}
+                total_market_value = sum(holdings_market_value.values())
+
+                # display portfolio book/market value and profit/loss in dollars
+                print(colored(f"P/L: ", "yellow", attrs=['bold']) + locale.currency(total_market_value - total_book_value) + " (" + colored(f"{(total_market_value - total_book_value)/total_book_value:.2%}", "green") + ")" + colored("\nBOOK: ", "yellow", attrs=['bold']) + locale.currency(total_book_value) + colored("\nMARKET: ", "yellow", attrs=['bold']) + locale.currency(total_market_value) + "\n")
+                print(colored("Symbol   Price       Balance              Value", "yellow", attrs=['bold']))
+                print(colored("---------------------------------------------------", "green", attrs=['bold']))
+
+                # display individual holdings in cryptocurrency and dollars
+                for i in symbols:
+                    spaces = (6 - len(i)) * ' '
+                    spaces2 = (10 - len(str(locale.currency(prices[i])))) * ' '
+                    spaces3 = (20 - len(str(holdings[i]))) * ' '
+                    print(colored(f"{i}:", "yellow", attrs=['bold']) + (f"{spaces}  {locale.currency(prices[i])} {spaces2} {holdings[i]} {spaces3}" + colored(locale.currency(holdings_market_value[i]), "blue", attrs=['bold'])))
+                time.sleep(15)
+
+    # prompt user for valid file until one is provided, then add it to config
+    else:
+        while not path.isfile(spreadsheet):
+            coinport_config_data['spreadsheet']['filepath'] = input("Woops, no spreadsheet found! Please specify filename: ")
+            spreadsheet = coinport_config_data['spreadsheet']['filepath']
+        with open(coinport_config, 'w') as config_file:
+            json.dump(coinport_config_data, config_file, sort_keys=True, indent=4, separators=(',', ': '))
